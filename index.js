fa534af44d1f97f053a403857498d6c395d97d19
Toby-28 <tobymarshal2802@gmail.com>
2022-03-02 17:11:17 +0500

    s

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 
 const router = require('./router')
 
+const PORT = 3004
+
 app.use(express.static('public'))
 app.set('views', 'views')
 app.set('view engine', 'ejs')
@@ -13,6 +15,6 @@ app.use((req, res) => {
   res.render('404.ejs')
 })
 
-app.listen(30022, () => {
-  console.log('port: 30022')
+app.listen(3004, () => {
+  console.log('port: ' + PORT)
 })
