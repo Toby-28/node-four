3870a71d48ed9af6ee286d37b225b8209686df4e
Toby-28 <tobymarshal2802@gmail.com>
2022-03-09 21:05:28 +0500

    s

--- router/back.js
+++ router/back.js
@@ -1,16 +1,16 @@
 const router = require('express').Router()
 
-const connection = require('../db/mysql')
+const connection = require('../db/postgreSQL')
 
-function getFormattedDate(results) {
-  results.forEach((result) => {
-    const date = result['birth_date']
+function getFormattedDate(rows) {
+  rows.forEach((row) => {
+    const date = row['birth_date']
     const option = {
       year: 'numeric',
       month: 'long',
       day: 'numeric',
     }
-    result['birth_date'] = date.toLocaleDateString('en-EN', option)
+    row['birth_date'] = date.toLocaleDateString('en-EN', option)
   })
 }
 
@@ -19,7 +19,7 @@ router.get('/dt_employees', (req, res) => {
     if (err) {
       console.log(err)
     }
-    getFormattedDate(results)
+    getFormattedDate(results.rows)
     res.json(results)
   })
 })
@@ -29,9 +29,25 @@ router.get('/dt_users', (req, res) => {
     if (err) {
       console.log(err)
     }
-    getFormattedDate(results)
+    getFormattedDate(results.rows)
     res.json(results)
   })
+})
+
+router.post('/dt_employees', (req, res) => {
+  const { name, surname, birthDate, hiredDate, jobId, salary } = req.body
+  connection.query(
+    `insert into employees(name, surname, birthDate, hiredDate, jobId, salary) 
+    values()`,
+    [name, surname, birthDate, hiredDate, jobId, salary],
+    (err, messages) => {
+      if (err) {
+        console.log(err)
+      }
+      console.log(messages)
+      res.end()
+    }
+  )
 })
 
 module.exports = router
